85de3bae4526073ca901ac2f5e95242be31581cd
Joel Speed <joel.speed@hotmail.co.uk>
2020-03-09 14:44:44 +0000

    UPSTREAM: <carry>: openshift: Add envTest tests for reconciler

--- pkg/cloud/azure/actuators/machineset/controller_test.go
+++ pkg/cloud/azure/actuators/machineset/controller_test.go
@@ -14,15 +14,179 @@ limitations under the License.
 package machineset
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
-	"github.com/ghodss/yaml"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	gtypes "github.com/onsi/gomega/types"
 	machinev1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
 	machineproviderv1 "sigs.k8s.io/cluster-api-provider-azure/pkg/apis/azureprovider/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
+
+var _ = Describe("Reconciler", func() {
+	var c client.Client
+	var stopMgr chan struct{}
+	var fakeRecorder *record.FakeRecorder
+	var namespace *corev1.Namespace
+
+	BeforeEach(func() {
+		mgr, err := manager.New(cfg, manager.Options{MetricsBindAddress: "0"})
+		Expect(err).ToNot(HaveOccurred())
+
+		r := Reconciler{
+			Client: mgr.GetClient(),
+			Log:    log.Log,
+		}
+		Expect(r.SetupWithManager(mgr, controller.Options{})).To(Succeed())
+
+		fakeRecorder = record.NewFakeRecorder(1)
+		r.recorder = fakeRecorder
+
+		c = mgr.GetClient()
+		stopMgr = StartTestManager(mgr)
+
+		namespace = &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{GenerateName: "mhc-test-"}}
+		Expect(c.Create(ctx, namespace)).To(Succeed())
+	})
+
+	AfterEach(func() {
+		Expect(deleteMachineSets(c, namespace.Name)).To(Succeed())
+		close(stopMgr)
+	})
+
+	type reconcileTestCase = struct {
+		vmSize              string
+		existingAnnotations map[string]string
+		expectedAnnotations map[string]string
+		expectedEvents      []string
+	}
+
+	DescribeTable("when reconciling MachineSets", func(rtc reconcileTestCase) {
+		machineSet, err := newTestMachineSet(namespace.Name, rtc.vmSize, rtc.existingAnnotations)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(c.Create(ctx, machineSet)).To(Succeed())
+
+		Eventually(func() map[string]string {
+			m := &machinev1.MachineSet{}
+			key := client.ObjectKey{Namespace: machineSet.Namespace, Name: machineSet.Name}
+			err := c.Get(ctx, key, m)
+			if err != nil {
+				return nil
+			}
+			annotations := m.GetAnnotations()
+			if annotations != nil {
+				return annotations
+			}
+			// Return an empty map to distinguish between empty annotations and errors
+			return make(map[string]string)
+		}, timeout).Should(Equal(rtc.expectedAnnotations))
+
+		// Check which event types were sent
+		Eventually(fakeRecorder.Events, timeout).Should(HaveLen(len(rtc.expectedEvents)))
+		receivedEvents := []string{}
+		eventMatchers := []gtypes.GomegaMatcher{}
+		for _, ev := range rtc.expectedEvents {
+			receivedEvents = append(receivedEvents, <-fakeRecorder.Events)
+			eventMatchers = append(eventMatchers, ContainSubstring(fmt.Sprintf(" %s ", ev)))
+		}
+		Expect(receivedEvents).To(ConsistOf(eventMatchers))
+	},
+		Entry("with no vmSize set", reconcileTestCase{
+			vmSize:              "",
+			existingAnnotations: make(map[string]string),
+			expectedAnnotations: make(map[string]string),
+			expectedEvents:      []string{"ReconcileError"},
+		}),
+		Entry("with a Standard_D4s_v3", reconcileTestCase{
+			vmSize:              "Standard_D4s_v3",
+			existingAnnotations: make(map[string]string),
+			expectedAnnotations: map[string]string{
+				cpuKey:    "4",
+				memoryKey: "16384",
+				gpuKey:    "0",
+			},
+			expectedEvents: []string{},
+		}),
+		Entry("with a Standard_NC24", reconcileTestCase{
+			vmSize:              "Standard_NC24",
+			existingAnnotations: make(map[string]string),
+			expectedAnnotations: map[string]string{
+				cpuKey:    "24",
+				memoryKey: "229376",
+				gpuKey:    "4",
+			},
+			expectedEvents: []string{},
+		}),
+		Entry("with existing annotations", reconcileTestCase{
+			vmSize: "Standard_NC24",
+			existingAnnotations: map[string]string{
+				"existing": "annotation",
+				"annother": "existingAnnotation",
+			},
+			expectedAnnotations: map[string]string{
+				"existing": "annotation",
+				"annother": "existingAnnotation",
+				cpuKey:     "24",
+				memoryKey:  "229376",
+				gpuKey:     "4",
+			},
+			expectedEvents: []string{},
+		}),
+		Entry("with an invalid vmSize", reconcileTestCase{
+			vmSize: "r4.xLarge",
+			existingAnnotations: map[string]string{
+				"existing": "annotation",
+				"annother": "existingAnnotation",
+			},
+			expectedAnnotations: map[string]string{
+				"existing": "annotation",
+				"annother": "existingAnnotation",
+			},
+			expectedEvents: []string{"ReconcileError"},
+		}),
+	)
+})
+
+func deleteMachineSets(c client.Client, namespaceName string) error {
+	machineSets := &machinev1.MachineSetList{}
+	err := c.List(ctx, machineSets, client.InNamespace(namespaceName))
+	if err != nil {
+		return err
+	}
+
+	for _, ms := range machineSets.Items {
+		err := c.Delete(ctx, &ms)
+		if err != nil {
+			return err
+		}
+	}
+
+	Eventually(func() error {
+		machineSets := &machinev1.MachineSetList{}
+		err := c.List(ctx, machineSets)
+		if err != nil {
+			return err
+		}
+		if len(machineSets.Items) > 0 {
+			return fmt.Errorf("MachineSets not deleted")
+		}
+		return nil
+	}, timeout).Should(Succeed())
+
+	return nil
+}
 
 func TestReconcile(t *testing.T) {
 	testCases := []struct {
@@ -96,7 +260,7 @@ func TestReconcile(t *testing.T) {
 		t.Run(tc.name, func(tt *testing.T) {
 			g := NewWithT(tt)
 
-			machineSet, err := newTestMachineSet(tc.vmSize, tc.existingAnnotations)
+			machineSet, err := newTestMachineSet("default", tc.vmSize, tc.existingAnnotations)
 			g.Expect(err).ToNot(HaveOccurred())
 
 			_, err = reconcile(machineSet)
@@ -106,7 +270,7 @@ func TestReconcile(t *testing.T) {
 	}
 }
 
-func newTestMachineSet(vmSize string, existingAnnotations map[string]string) (*machinev1.MachineSet, error) {
+func newTestMachineSet(namespace string, vmSize string, existingAnnotations map[string]string) (*machinev1.MachineSet, error) {
 	// Copy anntotations map so we don't modify the input
 	annotations := make(map[string]string)
 	for k, v := range existingAnnotations {
@@ -123,7 +287,9 @@ func newTestMachineSet(vmSize string, existingAnnotations map[string]string) (*m
 
 	return &machinev1.MachineSet{
 		ObjectMeta: metav1.ObjectMeta{
-			Annotations: annotations,
+			Annotations:  annotations,
+			GenerateName: "test-machineset-",
+			Namespace:    namespace,
 		},
 		Spec: machinev1.MachineSetSpec{
 			Template: machinev1.MachineTemplateSpec{
@@ -136,7 +302,7 @@ func newTestMachineSet(vmSize string, existingAnnotations map[string]string) (*m
 }
 
 func providerSpecFromMachine(in *machineproviderv1.AzureMachineProviderSpec) (machinev1.ProviderSpec, error) {
-	bytes, err := yaml.Marshal(in)
+	bytes, err := json.Marshal(in)
 	if err != nil {
 		return machinev1.ProviderSpec{}, err
 	}
