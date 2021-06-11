3323fac2c5b6b9263b9dd28c945c58560a866bdb
Denis Moiseev <dmoiseev@redhat.com>
2021-06-11 10:24:51 +0200

    UPSTREAM: <carry>: openshift: Improve logging message in case if vm not found

--- pkg/cloud/azure/services/virtualmachines/virtualmachines.go
+++ pkg/cloud/azure/services/virtualmachines/virtualmachines.go
@@ -88,7 +88,7 @@ func (s *Service) Get(ctx context.Context, spec azure.Spec) (interface{}, error)
 	}
 	vm, err := s.Client.Get(ctx, s.Scope.MachineConfig.ResourceGroup, vmSpec.Name, compute.InstanceView)
 	if err != nil && azure.ResourceNotFound(err) {
-		klog.Warningf("vm %s not found: %w", vmSpec.Name, err)
+		klog.Warningf("vm %s not found: %w", vmSpec.Name, err.Error())
 		return nil, err
 	} else if err != nil {
 		return vm, err
